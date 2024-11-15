a93498df2ba811b0bd58336fe397de669f09d695
vishawanath <vishu.bengaluru@gmail.com>
2024-11-15 14:06:54 +0530

    added user response

--- src/main/java/struggler/to/achiever/response/UserResponse.java
+++ src/main/java/struggler/to/achiever/response/UserResponse.java
@@ -1,4 +1,31 @@
 package struggler.to.achiever.response;
 
 public class UserResponse {
+
+    String username;
+    String password;
+
+    public UserResponse() {
+    }
+
+    public UserResponse(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
